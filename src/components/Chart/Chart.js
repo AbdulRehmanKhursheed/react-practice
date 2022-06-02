d1fa96f3a6caa00b17ab28fdf67822174aca3083
Abdul Rehman Khursheed <malik.ali5560@gmail.com>
2022-06-02 05:36:00 +0500

    updated

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,6 @@
 import ChartBar from "./ChartBar";
 import "./Chart.css";
 const Chart = props => {
-  console.log(props);
   const dataPointValue = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValue);
   return (
